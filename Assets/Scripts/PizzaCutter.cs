9f0b6f4eb6668357cefac10273c9c2621f7d3972
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-21 22:56:30 +0300

    Refactor project structure, added PizzaCutter class.

---
+++ Assets/Scripts/PizzaCutter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaCutter : MonoBehaviour
+{
+    private Mesh mesh;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        CutPizza(new List<Vector3>());
+    }
+
+    public void CutPizza(List<Vector3> points)
+    {
+        var vertices = mesh.vertices;
+
+        foreach(var vertice in vertices)
+        {
+            Debug.Log(vertice);
+        }
+    }
+}

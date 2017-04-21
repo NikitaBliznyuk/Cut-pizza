bd62982234b6b7f56250bc13297ae88d21da1fdf
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-22 01:38:39 +0300

    Now pizza succesfull cut with the input!

--- Assets/Scripts/PizzaCutter.cs
+++ Assets/Scripts/PizzaCutter.cs
@@ -20,7 +20,7 @@ public class PizzaCutter : MonoBehaviour
 
         vertexCount = meshFilter.mesh.vertexCount;
 
-        CutPizza(new List<Vector3>() { new Vector3(1.0f, -0.2f), new Vector3(0.45f, 0.2f)});
+        CutPizza(new List<Vector3>() { new Vector3(0.1f, 0.5f), new Vector3(0.45f, 0.2f) });
     }
 
     public void CutPizza(List<Vector3> input)
@@ -31,34 +31,80 @@ public class PizzaCutter : MonoBehaviour
         var nearestPoints = GetNearPoints(input, vertices);
 
         if (nearestPoints[0] == nearestPoints[1]) // to prevent tapping on screen
+        {
+            Debug.Log("Try again!");
             return;
+        }
 
-        var delta = Mathf.Abs(nearestPoints[0] - nearestPoints[1]);
-        var startIndex = delta > (vertexCount - delta) ? nearestPoints[0] : nearestPoints[1];
+        if (nearestPoints[0] > nearestPoints[1])
+        {
+            var temp = nearestPoints[0];
+            nearestPoints[0] = nearestPoints[1];
+            nearestPoints[1] = temp;
+        }
+
+        var delta = Mathf.Abs(nearestPoints[1] - nearestPoints[0]);
+        var startIndex = delta > (vertexCount - delta) ? nearestPoints[1] : nearestPoints[0];
         var endIndex = startIndex == nearestPoints[0] ? nearestPoints[1] : nearestPoints[0];
+        var startTriangleIndex = 0;
+        var endTriangleIndex = 0;
+
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            if (triangles[i] == startIndex)
+                startTriangleIndex = i;
+            else if (triangles[i + 1] == endIndex)
+                endTriangleIndex = i;
+        }
+
+        Debug.Log("Start indexes: " + startIndex + " " + endIndex);
+        Debug.Log("Start triangle indexes: " + startTriangleIndex + " " + endTriangleIndex
+            + "  VALUES: " + triangles[startTriangleIndex] + " " + triangles[endTriangleIndex + 1]);
 
         var newTriangles = new List<int>();
         var cuttedTriangles = new List<int>();
-        for(int k = 0, n = startIndex; k < triangles.Length; k += 3, n++)
-        {
-            n -= n >= vertexCount ? vertexCount : 0;
-            if(triangles[k] != n && triangles[k + 1] != n && triangles[k + 2] != n)
-            {
-                newTriangles.Add(triangles[k]);
-                newTriangles.Add(triangles[k + 1]);
-                newTriangles.Add(triangles[k + 2]);
 
-                Debug.Log("Not in!");
+        for (int i = 0; i < triangles.Length; i += 3)
+        {
+            if(startTriangleIndex < endTriangleIndex)
+            {
+                if(i >= startTriangleIndex && (i + 1) <= endTriangleIndex)
+                {
+                    cuttedTriangles.Add(triangles[i]);
+                    cuttedTriangles.Add(triangles[i + 1]);
+                    cuttedTriangles.Add(triangles[i + 2]);
+                }
+                else
+                {
+                    newTriangles.Add(triangles[i]);
+                    newTriangles.Add(triangles[i + 1]);
+                    newTriangles.Add(triangles[i + 2]);
+                }
             }
             else
             {
-                cuttedTriangles.Add(triangles[k]);
-                cuttedTriangles.Add(triangles[k + 1]);
-                cuttedTriangles.Add(triangles[k + 2]);
-
-                Debug.Log("In!");
+                if (i >= endTriangleIndex && (i + 1) <= startTriangleIndex)
+                {
+                    newTriangles.Add(triangles[i]);
+                    newTriangles.Add(triangles[i + 1]);
+                    newTriangles.Add(triangles[i + 2]);
+                }
+                else
+                {
+                    cuttedTriangles.Add(triangles[i]);
+                    cuttedTriangles.Add(triangles[i + 1]);
+                    cuttedTriangles.Add(triangles[i + 2]);
+                }
             }
         }
+
+        if (cuttedTriangles.Count == 0)
+        {
+            Debug.Log("Try again!");
+            return;
+        }
+
+        Debug.Log(newTriangles.Count);
 
         meshFilter.mesh.triangles = newTriangles.ToArray();
 
@@ -66,7 +112,7 @@ public class PizzaCutter : MonoBehaviour
         cuttedPart.transform.position = transform.position;
         var cuttedFilter = cuttedPart.GetComponent<MeshFilter>();
         var cuttedRenderer = cuttedPart.GetComponent<MeshRenderer>();
-        
+
         cuttedFilter.mesh.triangles = cuttedTriangles.ToArray();
     }
 
@@ -74,12 +120,12 @@ public class PizzaCutter : MonoBehaviour
     {
         var points = new int[input.Count];
 
-        for(int i = 0; i < input.Count; i++)
+        for (int i = 0; i < input.Count; i++)
         {
             float minDistance = (input[i] - vertices[0]).magnitude;
             int index = 0;
 
-            for(int j = 1; j < vertices.Length; j++)
+            for (int j = 1; j < vertices.Length; j++)
             {
                 var distance = (input[i] - vertices[j]).magnitude;
                 if (distance < minDistance && j != indexOfCenter)
